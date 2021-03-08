c22249f3cc8f4e0e82c04a5146b2c2beb0ad63ab
OscarPacheco2020 <oscar.rene.pc@gmail.com>
2021-03-08 16:15:41 -0600

    Adatacion e instalacion de liberias y creacion de componeste, servico y model

--- crud-segurt-front/src/app/app.module.ts
+++ crud-segurt-front/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//extra
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot() // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
