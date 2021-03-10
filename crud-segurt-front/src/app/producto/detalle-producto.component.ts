6d3664b60e9dcb8bede46b3d91089480f7ff47d8
OscarPacheco2020 <oscar.rene.pc@gmail.com>
2021-03-09 19:24:13 -0600

    Creacion de detalle y update

--- crud-segurt-front/src/app/producto/detalle-producto.component.ts
+++ crud-segurt-front/src/app/producto/detalle-producto.component.ts
@@ -1,4 +1,8 @@
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Producto } from '../models/producto';
+import { ProductoService } from '../service/producto.service';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleProductoComponent implements OnInit {
 
-  constructor() { }
+  producto: Producto;
+
+  constructor(
+    private productoService: ProductoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.detail(id).subscribe(
+      data => {
+        this.producto = data;
+      },
+      error => {
+        this.toastr.error(error.error.mensaje, 'Fail', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.volver();
+      }
+    );
+  }
+
+  volver(): void {
+    this.router.navigate(['/']);
   }
 
 }
--- crud-segurt-front/src/app/producto/editar-producto.component.ts
+++ crud-segurt-front/src/app/producto/editar-producto.component.ts
@@ -1,4 +1,8 @@
+import { ToastrService } from 'ngx-toastr';
+import { ProductoService } from './../service/producto.service';
+import { Producto } from './../models/producto';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-producto',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarProductoComponent implements OnInit {
 
-  constructor() { }
+  producto: Producto;
+  colorLabel: boolean = true;
+
+  constructor(
+    private productoService: ProductoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.detail(id).subscribe(
+      data => {
+        this.producto = data;
+      },
+      error => {
+        this.toastr.error(error.error.mensaje, 'Fail', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/']);
+      }
+    );
+  }
+
+  onUpdate(): void {
+    if (this.producto.precio >= 0) {
+      this.colorLabel = true;
+      const id = this.activatedRoute.snapshot.params.id;
+      this.productoService.updata(id, this.producto).subscribe(
+        data => {
+          this.toastr.success('Producto Actualizado', 'Ok', {
+            timeOut: 3000, positionClass: 'toast-top-center'
+          });
+          this.router.navigate(['/']);
+        },
+        error => {
+          this.toastr.error(error.error.mensaje, 'Fail', {
+            timeOut: 3000, positionClass: 'toast-top-center'
+          });
+          this.router.navigate(['/']);
+        }
+      );
+    } else {
+      this.colorLabel = false;
+    }
+
   }
 
 }
