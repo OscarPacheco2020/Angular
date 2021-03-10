4a54bdb2165477cec862346da2648201e2608b25
OscarPacheco2020 <oscar.rene.pc@gmail.com>
2021-03-09 18:29:19 -0600

    Modificacion de app-routting.module.ts, app.component.html, lista-producto.componet y nueva para mostrar, nuevo y eliminar

--- crud-segurt-front/src/app/producto/lista-producto.component.ts
+++ crud-segurt-front/src/app/producto/lista-producto.component.ts
@@ -1,4 +1,7 @@
+import { ProductoService } from './../service/producto.service';
+import { Producto } from './../models/producto';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-lista-producto',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProductoComponent implements OnInit {
 
-  constructor() { }
+  productos: Producto[] =  [];
+
+  constructor(
+    private productoService: ProductoService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.cargarProducto();
+  }
+
+  cargarProducto(): void{
+    this.productoService.lista().subscribe(
+      data => {
+        this.productos = data;
+      },
+      error => {
+        console.log('Error: ' + error);
+      }
+    );
+  }
+
+  borrar(id: number){
+    this.productoService.delete(id).subscribe(
+      data => {
+        this.toastr.success('Producto Eliminado', 'Ok', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.cargarProducto();
+      },
+      error => {
+        this.toastr.error(error.error.mensaje, 'Fail', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+
+      }
+    );
   }
 
 }
--- crud-segurt-front/src/app/producto/nuevo-producto.component.ts
+++ crud-segurt-front/src/app/producto/nuevo-producto.component.ts
@@ -1,4 +1,8 @@
+import { Producto } from './../models/producto';
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../service/producto.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuevo-producto',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoProductoComponent implements OnInit {
 
-  constructor() { }
+  nombre = '';
+  precio: number = null;
+  colorLabel: boolean = true;
+
+  constructor(
+    private productoService: ProductoService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(): void {
+
+    if (this.precio >= 0) {
+      this.colorLabel = true;
+
+      const producto = new Producto(this.nombre, this.precio);
+      this.productoService.save(producto).subscribe(
+        data => {
+          this.toastr.success('Producto Creado', 'Ok', {
+            timeOut: 3000, positionClass: 'toast-top-center'
+          });
+          this.router.navigate(['/']);
+        },
+        error => {
+          this.toastr.error(error.error.mensaje, 'Fail', {
+            timeOut: 3000, positionClass: 'toast-top-center'
+          });
+          this.router.navigate(['/']);
+        }
+      );
+    } else {
+      this.colorLabel = false;
+    }
+
   }
 
 }
