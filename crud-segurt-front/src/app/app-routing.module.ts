70fb00f1bd94901a78f20ffb360d6d0f1c026223
OscarPacheco2020 <oscar.rene.pc@gmail.com>
2021-03-11 11:04:28 -0600

    Estrutura para aplicar seguridad

--- crud-segurt-front/src/app/app-routing.module.ts
+++ crud-segurt-front/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
+import { RegistroComponent } from './auth/registro.component';
+import { LoginComponent } from './auth/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 import { DetalleProductoComponent } from './producto/detalle-producto.component';
 import { EditarProductoComponent } from './producto/editar-producto.component';
 import { ListaProductoComponent } from './producto/lista-producto.component';
 import { NuevoProductoComponent } from './producto/nuevo-producto.component';
 
 const routes: Routes = [
-  { path: '', component: ListaProductoComponent },
+  { path: '', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'lista', component: ListaProductoComponent },
   { path: 'detalle/:id', component: DetalleProductoComponent },
   { path: 'nuevo', component: NuevoProductoComponent },
   { path: 'editar/:id', component: EditarProductoComponent },
